db9b73eccd1ca210e10df66c357f69bfcc78b354
niravajmeri <nirav.ajmeri@gmail.com>
2017-09-16 16:47:26 -0400

    DBSCAN with word2vec

--- cluster-dbscan.py
+++ cluster-dbscan.py
@@ -16,6 +16,8 @@ from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
 
+google = gensim.models.word2vec.Word2Vec.load_word2vec_format('/home/word2vec-models/GoogleNews-vectors-negative300.bin', binary=True)
+
 
 with open('data/requirements.txt', 'r') as myfile1:
     data1=myfile1.read().replace('\n', '')
@@ -30,9 +32,9 @@ print 'Stemmed text1: %s \n\n\n' % s1
 
 lemma = WordNetLemmatizer()
 
-lemma_text1 = [lemma.lemmatize(i, pos="n") for i in word_tokenize(data1)]
+lemma_text = [lemma.lemmatize(i, pos="n") for i in word_tokenize(data1)]
 
-ls1 = ' '. join(lemma_text1)
+ls1 = ' '. join(lemma_text)
 
 print 'Lemma text1: %s \n\n\n' % ls1
 
@@ -47,15 +49,54 @@ stemmed_docs = [s1]
 
 lemmatized_docs = [ls1]
 
-vectorizer = TfidfVectorizer(stop_words='english')
-X = vectorizer.fit_transform(lemmatized_docs)
+#vectorizer = TfidfVectorizer(stop_words='english')
+#X = vectorizer.fit_transform(lemmatized_docs)
+
+seen = set()
+result = []
+for item in lemma_text:
+    if item not in seen:
+        seen.add(item)
+        result.append(item)
+
+
+fails = []
+hits = []
+vectors = []
+for keyword in result:
+  try:
+    google.vocab[keyword]
+    #freebase.vocab['/en/' + keyword]
+    vectors.append(google[keyword])
+    #vectors.append(freebase['/en/' + keyword])
+    hits.append(keyword)
+  except:
+    try:
+      unigrams = keyword.split('_')
+      uni_vectors = []
+      for uni in unigrams:
+        #freebase.vocab['/en/' + uni]
+        google.vocab[uni]
+        #uni_vectors.append(freebase['/en/' + uni])
+        uni_vectors.append(google[uni])
+      vectors.append(sum(uni_vectors) / len(unigrams))
+      hits.append(keyword)
+
+    except:
+      fails.append(keyword)
+
+
+# Standaridze
+new_vectors = StandardScaler().fit_transform(vectors)
+
+
 
 # Try out various eps to see distribution of clusters
 # Adjust eps bounds and step size
 for eps in np.arange(13,14.5,0.1):
   print 'Eps: '
   print eps
-  new_db = DBSCAN(eps=eps, min_samples=10).fit(X)
+  new_db = DBSCAN(eps=eps, min_samples=10).fit(new_vectors)
   labels = new_db.labels_
   counter=collections.Counter(labels)
   print counter
@@ -64,7 +105,7 @@ for eps in np.arange(13,14.5,0.1):
 
 
 # Once you found a decent eps, use it here
-new_db = DBSCAN(eps=13.9, min_samples=10).fit(X)
+new_db = DBSCAN(eps=13.9, min_samples=10).fit(new_vectors)
 labels = new_db.labels_
 counter=collections.Counter(labels)
 
