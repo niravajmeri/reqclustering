24d6e56c5c610f1c6cc0a67f2568d2ec622c8f07
niravajmeri <nirav.ajmeri@gmail.com>
2017-09-16 16:25:16 -0400

    DBSCAN initial commit

---
+++ cluster-dbscan.py
@@ -0,0 +1,71 @@
+from nltk.stem.porter import *
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+from gensim.models import Word2Vec
+
+
+
+with open('data/requirements.txt', 'r') as myfile1:
+    data1=myfile1.read().replace('\n', '')
+
+stemmer = PorterStemmer()
+
+stemmed_text1 = [stemmer.stem(i) for i in word_tokenize(data1)]
+
+s1 = ' '.join(stemmed_text1)
+
+print 'Stemmed text1: %s \n\n\n' % s1
+
+lemma = WordNetLemmatizer()
+
+lemma_text1 = [lemma.lemmatize(i, pos="n") for i in word_tokenize(data1)]
+
+ls1 = ' '. join(lemma_text1)
+
+print 'Lemma text1: %s \n\n\n' % ls1
+
+with open("data/ls1.txt", 'w') as f:
+  f.write(ls1)
+
+#print 'Text1 %s' % string.join(stemmed_text1, " ")
+
+documents = [data1]
+
+stemmed_docs = [s1]
+
+lemmatized_docs = [ls1]
+
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(lemmatized_docs)
+
+# Try out various eps to see distribution of clusters
+# Adjust eps bounds and step size
+for eps in np.arange(13,14.5,0.1):
+  print 'Eps: '
+  print eps
+  new_db = DBSCAN(eps=eps, min_samples=10).fit(X)
+  labels = new_db.labels_
+  counter=collections.Counter(labels)
+  print counter
+  print len(counter)
+
+
+
+# Once you found a decent eps, use it here
+new_db = DBSCAN(eps=13.9, min_samples=10).fit(X)
+labels = new_db.labels_
+counter=collections.Counter(labels)
+
+clusters = {}
+for key, value in counter.iteritems():
+  clusters[key] = []
+
+
+for idx, val in enumerate(labels):
+  clusters[val].append(hits[idx])
+
+pprint(clusters)
