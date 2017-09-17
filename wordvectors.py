6921a6d391b676385df77c27ccaa6a58b09ce156
niravajmeri <nirav.ajmeri@gmail.com>
2017-09-17 15:51:41 -0400

    Get wordvectors

--- wordvectors.py
+++ wordvectors.py
@@ -4,21 +4,26 @@ from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import word_tokenize
 
-with open('data/group-36.csv', 'r') as myfile1:
-    data1=myfile1.read().replace('\n', '')
+with open('data/requirements.txt', 'r') as myfile1:
+    data=myfile1.read().replace('\n', '')
+
+data = data.lower
 
 lemma = WordNetLemmatizer()
 
-lemma_text1 = [lemma.lemmatize(i, pos="n") for i in word_tokenize(data1)]
+lemma_text = [lemma.lemmatize(i, pos="n") for i in word_tokenize(data)]
+
+# Remove stopwords
+stops = set(stopwords.words("english"))
+lemma_filtered = [word for word in lemma_text if word not in stops]
 
 #model = gensim.models.Word2Vec.load_word2vec_format('~/word2vec-model/GoogleNews-vectors-negative300.bin', binary=True) 
 
 model = gensim.models.KeyedVectors.load_word2vec_format('~/word2vec-model/GoogleNews-vectors-negative300.bin', binary=True) 
 
 #print lemma_text1[1]
-
 #print model[lemma_text1[1]]
 
-vectorfile = [model[lemma.lemmatize(i, pos="n")] for i in word_tokenize(data1)]
+vectorfile = [model[lemma.lemmatize(i, pos="n")] for i in word_tokenize(data)]
 
 print vectorfile
