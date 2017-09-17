3d45ce26c186444e82b505e53d5e913a0afd8af4
Nirav Ajmeri <nirav.ajmeri@gmail.com>
2017-09-17 16:55:55 -0400

    lower+strip+lemmatize+word2vec working

--- wordvectors.py
+++ wordvectors.py
@@ -1,29 +1,46 @@
+import string 
+
 import gensim
 from gensim.models import Word2Vec
 from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import word_tokenize
+from nltk.corpus import stopwords
 
-with open('data/requirements.txt', 'r') as myfile1:
-    data=myfile1.read().replace('\n', '')
+with open('data/requirements.txt', 'r') as myfile:
+    data=myfile.read().replace('\n', '')
 
-data = data.lower
+data = data.lower()
 
 lemma = WordNetLemmatizer()
 
-lemma_text = [lemma.lemmatize(i, pos="n") for i in word_tokenize(data)]
-
 # Remove stopwords
 stops = set(stopwords.words("english"))
-lemma_filtered = [word for word in lemma_text if word not in stops]
-
-#model = gensim.models.Word2Vec.load_word2vec_format('~/word2vec-model/GoogleNews-vectors-negative300.bin', binary=True) 
+#lemma_filtered = [word for word in lemma_text if word not in stops]
 
 model = gensim.models.KeyedVectors.load_word2vec_format('~/word2vec-model/GoogleNews-vectors-negative300.bin', binary=True) 
 
-#print lemma_text1[1]
-#print model[lemma_text1[1]]
+lemma_filtered = []
+vectors = []
 
-vectorfile = [model[lemma.lemmatize(i, pos="n")] for i in word_tokenize(data)]
+for word in word_tokenize(data):
+  word = word.lower()
+  word = word.strip()
+  word = word.strip('_')
+  word = word.strip('*')
+  
+  if word in stops:
+    continue
+  if all(char in string.punctuation for char in word):
+    continue
+ 
+  try:  
+    lemma_word = lemma.lemmatize(word, pos='n')
+    lemma_filtered.append(lemma_word)
+    vectors.append(model[lemma_word])
+  except KeyError:
+    print word, " not in vocabulary"
 
-print vectorfile
+print lemma_filtered
+
+print vectors
