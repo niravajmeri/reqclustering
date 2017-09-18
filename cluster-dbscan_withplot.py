5968f634481ebb943fa14308083802f6a3c85057
niravajmeri <nirav.ajmeri@gmail.com>
2017-09-17 21:47:29 -0400

    dbscan with plot

--- cluster-dbscan_withplot.py
+++ cluster-dbscan_withplot.py
@@ -1,3 +1,9 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.ioff()
+
+import string
 import gensim
 import sys
 import numpy as np
@@ -24,50 +30,59 @@ from pprint import pprint
 google = gensim.models.KeyedVectors.load_word2vec_format('~/word2vec-model/GoogleNews-vectors-negative300.bin', binary=True)
 
 
-with open('data/requirements.txt', 'r') as myfile1:
-    data1=myfile1.read().replace('\n', '')
+with open('data/requirements.txt', 'r') as myfile:
+  data=myfile.read().replace('\n', ' ')
 
-stemmer = PorterStemmer()
-
-stemmed_text1 = [stemmer.stem(i) for i in word_tokenize(data1)]
-
-s1 = ' '.join(stemmed_text1)
-
+#stemmer = PorterStemmer()
+#stemmed_text1 = [stemmer.stem(i) for i in word_tokenize(data1)]
+#s1 = ' '.join(stemmed_text1)
 #print 'Stemmed text1: %s \n\n\n' % s1
 
 lemma = WordNetLemmatizer()
-
-lemma_text = [lemma.lemmatize(i, pos="n") for i in word_tokenize(data1)]
+#lemma_text = [lemma.lemmatize(i, pos="n") for i in word_tokenize(data1)]
 
 # Remove stopwords
 stops = set(stopwords.words("english"))
-lemma_filtered = [word for word in lemma_text if word not in stops]
+#lemma_filtered = [word for word in lemma_text if word not in stops]
 
-ls1 = ' '. join(lemma_text)
+#ls1 = ' '. join(lemma_text)
 
 #print 'Lemma text1: %s \n\n\n' % ls1
 
-with open("data/ls1.txt", 'w') as f:
-  f.write(ls1)
+#with open("data/ls1.txt", 'w') as f:
+#  f.write(ls1)
 
 #print 'Text1 %s' % string.join(stemmed_text1, " ")
 
-documents = [data1]
-
-stemmed_docs = [s1]
-
-lemmatized_docs = [ls1]
+#documents = [data1]
+#stemmed_docs = [s1]
+#lemmatized_docs = [ls1]
 
 #vectorizer = TfidfVectorizer(stop_words='english')
 #X = vectorizer.fit_transform(lemmatized_docs)
 
 seen = set()
 result = []
-for item in lemma_filtered:
-    if item not in seen:
-        seen.add(item)
-        result.append(item)
-
+for word in word_tokenize(data):
+  word = word.lower()
+  word = word.strip()
+  word = word.strip('_')
+  word = word.strip('*')
+  
+  if word in stops:
+    continue
+  if all(char in string.punctuation for char in word):
+    continue
+  if len(word) < 3:
+    continue
+  try:
+    #stem_word = stemmer.stem(word)
+    lemma_word = lemma.lemmatize(word, pos='n')
+    if lemma_word not in seen:
+      seen.add(lemma_word)
+      result.append(lemma_word)
+  except KeyError:
+    print lemma_word, " not in vocabulary"
 
 fails = []
 hits = []
@@ -103,22 +118,26 @@ new_vectors = StandardScaler().fit_transform(vectors)
 # Try out various eps to see distribution of clusters
 # Adjust eps bounds and step size
 
-for eps in np.arange(14.5,15.5,0.1):
+'''
+for eps in np.arange(16.5,17.5,0.1):
   print 'Eps: '
   print eps
-  new_db = DBSCAN(eps=eps, min_samples=10).fit(new_vectors)
-  labels = new_db.labels_
-  counter=collections.Counter(labels)
-  print counter
-  print len(counter)
+  for min in np.arange(2,4,1):
+    print 'Min samples: '
+    print min
+    new_db = DBSCAN(eps=eps, min_samples=min).fit(new_vectors)
+    labels = new_db.labels_
+    counter=collections.Counter(labels)
+    print counter
+    print len(counter)
   
-  n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-  print('Estimated number of clusters: %d' % n_clusters_)
-
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    print('Estimated number of clusters: %d' % n_clusters_)
+'''
 
 
 # Once you found a decent eps, use it here
-new_db = DBSCAN(eps=14.75, min_samples=100).fit(new_vectors)
+new_db = DBSCAN(eps=16.5, min_samples=3).fit(new_vectors)
 core_samples_mask = np.zeros_like(new_db.labels_, dtype=bool)
 core_samples_mask[new_db.core_sample_indices_] = True
 labels = new_db.labels_
@@ -135,7 +154,7 @@ for key, value in counter.iteritems():
 for idx, val in enumerate(labels):
   clusters[val].append(hits[idx])
 
-with open('out-dbscan+plot.txt', 'wt') as out: 
+with open('out-cluster-dbscan-eps16.5-min3.txt', 'wt') as out: 
   pprint(clusters, stream=out)
 
 print('Estimated number of clusters: %d' % n_clusters_)
@@ -151,7 +170,7 @@ print("Silhouette Coefficient: %0.3f"
 
 ##############################################################################
 # Plot result
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # Black removed and is used for noise instead.
 unique_labels = set(labels)
@@ -172,4 +191,4 @@ for k, col in zip(unique_labels, colors):
              markeredgecolor='k', markersize=6)
 
 plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.savefig('dbscan+plt.png')
+plt.savefig('out-cluster-dbscan-eps16.5-min.png')
