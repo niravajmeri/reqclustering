9b4ecc2c9cd0c6c6ccbdc1337eb57ee22bcc265e
niravajmeri <nirav.ajmeri@gmail.com>
2017-09-17 23:48:05 -0400

    dbscan with plot updated

--- cluster-dbscan_withplot.py
+++ cluster-dbscan_withplot.py
@@ -174,20 +174,21 @@ print("Silhouette Coefficient: %0.3f"
 
 # Black removed and is used for noise instead.
 unique_labels = set(labels)
-colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
 for k, col in zip(unique_labels, colors):
-  if k == -1:
-  # Black used for noise.
-    col = [0, 0, 0, 1]
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
 
-  class_member_mask = (labels == k)
+    class_member_mask = (labels == k)
 
   xy = new_vectors[class_member_mask & core_samples_mask]
-  plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col, 
+  plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), 
     markeredgecolor='k', markersize=14)
 
   xy = new_vectors[class_member_mask & ~core_samples_mask]
-  plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+  plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
     markeredgecolor='k', markersize=6)
 
 plt.title('Estimated number of clusters: %d' % n_clusters_)
